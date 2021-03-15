a8c2a744e88547c308a882386676dff8efcb116e
chlghtod <chlghtod@gmail.com>
2021-03-15 15:55:23 +0900

    각자 개인별 스테이지 만들

---
+++ Assets/Scripts/Common/AppManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppManager : MonoSingleton<AppManager>
+{
+    public void Awake() 
+    {
+    }
+}
---
+++ Assets/Scripts/Common/InputManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoSingleton<InputManager>
+{
+    public void Awake() 
+    {
+    }
+}
