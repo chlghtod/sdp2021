f66c198a1b5727e4bc5d53583f72bafab159a660
chlghtod <chlghtod@gmail.com>
2021-03-29 15:15:16 +0900

    도관목Scene 이름 오류 수정

--- Assets/Scripts/도관목Scene.cs
+++ Assets/Scripts/도관목Scene.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class µµ°ü¸ñScene : MonoBehaviour
+public class ë„ê´€ëª©Scene : MonoBehaviour
 {
     void Update()
     {
