17b535cd2b784c86d2b87b6b6f5990b7db3e01ef
jungan <woorija1@gmail.com>
2021-03-29 15:54:06 +0900

    GG171003 선정안 씬,스크립트 이름 수정

--- Assets/Scripts/GG171003Scene.cs
+++ Assets/Scripts/GG171003Scene.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ¼±Á¤¾ÈScene : MonoBehaviour
+public class GG171003Scene : MonoBehaviour
 {
 
     void Update()
