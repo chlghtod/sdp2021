bc5f343efce9bf8eb263c868d35150006f28e0b0
DO\quseh <>
2021-03-29 16:12:38 +0900

    no message

--- Assets/Scripts/GG141005/GG141005Manager.cs
+++ Assets/Scripts/GG141005/GG141005Manager.cs
@@ -6,10 +6,6 @@ using UnityEngine.SceneManagement;
 public class GG141005Manager : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
--- Assets/Scripts/GG171003Scene.cs
+++ Assets/Scripts/GG171003Scene.cs
@@ -4,12 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GG171003Scene : MonoBehaviour
-<<<<<<< HEAD
 { 
-=======
-{
->>>>>>> ed3dc7188121d07ff4240e55ce5e932fe3f11e75
-
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
