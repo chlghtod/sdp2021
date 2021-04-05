bc7388c499a4976748a028fdee5fb624436fda2d
chlghtod <chlghtod@gmail.com>
2021-04-05 13:33:27 +0900

    State Pattern 샘플 스크립트 추가 Doc 폴더 추가

---
+++ Assets/Scripts/Sample/StatePattern/JustPattern/Context.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Context 클래스
+// 몬스터 클래스
+public class Monster
+{
+    private JustState state;
+
+    // Constructor
+    public Monster(JustState state)
+    {
+        this.state = state;
+    }
+
+    public void setState(JustState state)
+    {
+        this.state = state;
+    }
+
+    public void act()
+    {
+        state.Action();
+    }
+};
---
+++ Assets/Scripts/Sample/StatePattern/JustPattern/JustState.cs
@@ -0,0 +1,5 @@
+﻿// 상태 인터페이스 클래스
+public interface JustState
+{
+    void Action();
+};
